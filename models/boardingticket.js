01da7da5eb212062e3735bcd1d48305969d539bb
James Brooks <virverum22@gmail.com>
2024-02-10 13:28:26 -0500

    Finished Book prototype w/o adding metrics

--- models/boardingticket.js
+++ models/boardingticket.js
@@ -5,61 +5,71 @@ module.exports = (sequelize, DataTypes) => {
       this.Customer = this.belongsTo(models['Customer']);
       this.FlightSchedule = this.belongsTo(models['FlightSchedule']);
     }
-  };
+  }
   BoardingTicket.init(
     {
       seat: {
         type: DataTypes.STRING,
         validate: {
           notEmpty: {
-            msg: "Please enter in a valid seating arrangement",
-          }
-        }
+            msg: 'Please enter in a valid seating arrangement',
+          },
+        },
       },
       cost: {
-        type: DataTypes.DECIMAL(7, 2)
+        type: DataTypes.DECIMAL(7, 2),
       },
       isEmployee: {
         type: DataTypes.VIRTUAL,
         async get() {
           const customer = await this.getCustomer();
-          if (!customer || !customer.email)
-            return false;
+          if (!customer || !customer.email) return false;
           return customer.email.endsWith('avalonairlines');
-        }
-      }
-    }, {
+        },
+      },
+    },
+    {
       sequelize,
       modelName: 'BoardingTicket',
-    });
-    // Employees should be able to fly for free
-    BoardingTicket.beforeValidate('checkEmployee',
-      (ticket, options) => {
-        if (ticket.isEmployee) {
-          ticket.subtotal = 0;
-        }
+    }
+  );
+  // Employees should be able to fly for free
+  BoardingTicket.beforeValidate('checkEmployee', (ticket, options) => {
+    if (ticket.isEmployee) {
+      ticket.subtotal = 0;
+    }
+  });
+  // Subtotal should never be less than zero
+  BoardingTicket.beforeSave('checkSubtotal', (ticket, options) => {
+    if (ticket.subtotal < 0) {
+      throw new Error('Invalid subtotal for this ticket.');
+    }
+  });
+  //Ensure that the seat the customer has requested is available
+  BoardingTicket.beforeSave('checkSeat', async (ticket, options) => {
+    const newSeat = ticket.getDataValue('seat');
+    const { transaction } = options;
+    if (ticket.changed('seat')) {
+      const boardingTicketExists = await BoardingTicket.findOne({
+        where: {
+          seat: newSeat,
+        },
+        transaction,
       });
-      // Subtotal should never be less than zero
-      BoardingTicket.beforeSave('checkSubtotal', (ticket, options) => {
-        if (ticket.subtotal < 0) {
-          throw new Error('Invalid subtotal for this ticket.');
-        }
-      });
-      //Ensure that the seat the customer has requested is available
-      BoardingTicket.beforeSave('checkSeat', async (ticket, options) => {
-        const newSeat = ticket.getDataValue('seat');
-        const { transaction } = options;
-        if (ticket.changed('seat')) {
-          const boardingTicketExists = await BoardingTicket.findOne({
-            where: {
-              seat: newSeat
-            },
-            transaction,
-          });
-          if (boardingTicketExists !== null) {
-            throw new Error(`The seat ${newSeat} has already been taken.`);
-          }
-        }
-      });
+      if (boardingTicketExists !== null) {
+        throw new Error(`The seat ${newSeat} has already been taken.`);
+      }
+    }
+  });
+  BoardingTicket.afterSave('saveReceipt', async (ticket, options) => {
+    await sequelize.models.Receipts.create(
+      {
+        receipt: ticket.get(),
+      },
+      {
+        transaction: options.transaction,
+      }
+    );
+  });
   return BoardingTicket;
 };
